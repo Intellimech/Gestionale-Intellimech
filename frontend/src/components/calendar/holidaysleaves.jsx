822b12bd1f13546ec6823ba7f9a2ff072d29f065
fgervasoni7 <145766260+fgervasoni7@users.noreply.github.com>
2024-10-16 13:40:05 +0200

    feat!: ✨ Holidayleaves filters

--- frontend/src/components/calendar/holidaysleaves.jsx
+++ frontend/src/components/calendar/holidaysleaves.jsx
@@ -1,8 +1,6 @@
-import { Fragment, useState, useRef, useEffect } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState, useEffect } from 'react';
 import { XMarkIcon, CheckIcon, ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid';
 import axios from 'axios';
-import Cookies from 'js-cookie';
 import toast, { Toaster } from 'react-hot-toast';
 
 function classNames(...classes) {
@@ -11,9 +9,8 @@ function classNames(...classes) {
 
 export default function HolidaysLeavesManagement({ permissions }) {
   const [calendars, setCalendars] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [sortColumn, setSortColumn] = useState('');
-  const [sortDirection, setSortDirection] = useState('asc');
+  const [sortColumn, setSortColumn] = useState(''); // Default empty
+  const [sortDirection, setSortDirection] = useState('asc'); // Default ascending
   const [searchQueries, setSearchQueries] = useState({
     location: '',
     date: '',
@@ -26,6 +23,7 @@ export default function HolidaysLeavesManagement({ permissions }) {
     fetchCalendars();
   }, []);
 
+  // Fetch data from the API
   const fetchCalendars = () => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/holidays-leaves/read/`)
@@ -37,49 +35,48 @@ export default function HolidaysLeavesManagement({ permissions }) {
       });
   };
 
+  // Approve request
   const approveCalendar = (id) => {
     toast.promise(
       axios
         .post(`${process.env.REACT_APP_API_URL}/holidays-leaves/approve/${id}`)
-        .then((response) => {
-          fetchCalendars();
-        })
+        .then(() => fetchCalendars())
         .catch((error) => {
           console.log(error);
-          throw new Error('Error approving calendar');
+          throw new Error('Error while approving');
         }),
       {
-        loading: 'Approvando...',
-        success: 'Approvata con successo!',
-        error: 'Errore nell\'approvare la richiesta',
+        loading: 'Approving...',
+        success: 'Successfully approved!',
+        error: 'Error in approving',
       }
     );
   };
 
+  // Reject request
   const rejectCalendar = (id) => {
     toast.promise(
       axios
         .post(`${process.env.REACT_APP_API_URL}/holidays-leaves/reject/${id}`)
-        .then((response) => {
-          fetchCalendars();
-        })
+        .then(() => fetchCalendars())
         .catch((error) => {
           console.log(error);
-          throw new Error('Error rejecting calendar');
+          throw new Error('Error while rejecting');
         }),
       {
-        loading: 'Rifiutando...',
-        success: 'Rifiutata con successo!',
-        error: 'Errore nel rifiutare la richiesta',
+        loading: 'Rejecting...',
+        success: 'Successfully rejected!',
+        error: 'Error in rejecting',
       }
     );
   };
 
-  const handleSort = (columnName) => {
-    if (sortColumn === columnName) {
+  // Sorting logic
+  const handleSort = (column) => {
+    if (sortColumn === column) {
       setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
     } else {
-      setSortColumn(columnName);
+      setSortColumn(column);
       setSortDirection('asc');
     }
   };
@@ -94,20 +91,23 @@ export default function HolidaysLeavesManagement({ permissions }) {
     }
   };
 
-  const filteredCalendars = calendars.filter((item) => {
-    return (
-      (searchQueries.location === '' || item.location.toLowerCase().includes(searchQueries.location.toLowerCase())) &&
-      (searchQueries.date === '' || item.date.includes(searchQueries.date)) &&
-      (searchQueries.period === '' || item.period.toLowerCase().includes(searchQueries.period.toLowerCase())) &&
-      (searchQueries.status === '' || item.status.toLowerCase().includes(searchQueries.status.toLowerCase())) &&
-      (searchQueries.owner === '' || item.owner.toString().includes(searchQueries.owner))
-    );
-  });
+  // Filtering and sorting the data
+  const filteredCalendars = calendars
+    .filter((item) => item.status === 'In Attesa di Approvazione') // Show only "In Attesa di Approvazione"
+    .filter((item) => {
+      return (
+        (searchQueries.location === '' || item.Location.name.toLowerCase().includes(searchQueries.location.toLowerCase())) &&
+        (searchQueries.date === '' || item.date.includes(searchQueries.date)) &&
+        (searchQueries.period === '' || item.period.toLowerCase().includes(searchQueries.period.toLowerCase())) &&
+        (searchQueries.status === '' || item.status.toLowerCase().includes(searchQueries.status.toLowerCase())) &&
+        (searchQueries.owner === '' || (item.ownerUser?.name + ' ' + item.ownerUser?.surname).toLowerCase().includes(searchQueries.owner.toLowerCase()))
+      );
+    });
 
   const sortedCalendars = filteredCalendars.sort((a, b) => {
     const valueA = a[sortColumn];
     const valueB = b[sortColumn];
-  
+
     if (sortDirection === 'asc') {
       return compareValues(valueA, valueB);
     } else {
@@ -115,10 +115,12 @@ export default function HolidaysLeavesManagement({ permissions }) {
     }
   });
 
+  // Handle search input changes
   const handleSearchInputChange = (column) => (event) => {
     setSearchQueries({ ...searchQueries, [column]: event.target.value });
   };
 
+  // Export data to CSV
   const exportData = () => {
     const csvContent =
       'data:text/csv;charset=utf-8,' +
@@ -130,23 +132,23 @@ export default function HolidaysLeavesManagement({ permissions }) {
     document.body.appendChild(link);
     link.click();
   };
+
   return (
     <>
       <Toaster position="bottom-right" reverseOrder={false} />
-     
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="py-4">
           <div className="flex items-center justify-between">
             <div className="sm:flex-auto">
-              <h1 className="text-base font-semibold leading-6 text-gray-900">Ferie e Permessi</h1>
-              <p className="mt-2 text-sm text-gray-700">Lista delle richieste di ferie e permessi</p>
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Ferie e Permessi</h1>
+            <p className="mt-2 text-sm text-gray-700">Lista delle richieste di ferie e permessi</p>
             </div>
             <div className="flex items-center space-x-4">
               <button
                 onClick={exportData}
-                className="block rounded-md bg-[#A7D0EB] px-2 py-1 text-center text-xs font-bold leading-5 text-black shadow-sm hover:bg-[#7fb7d4] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#7fb7d4]"
+                className="block rounded-md bg-[#A7D0EB] px-2 py-1 text-center text-xs font-bold leading-5 text-black shadow-sm hover:bg-[#7fb7d4]"
               >
-                Esporta
+                Export
               </button>
             </div>
           </div>
@@ -156,38 +158,46 @@ export default function HolidaysLeavesManagement({ permissions }) {
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
               <div className="relative">
                 <table className="min-w-full table-fixed divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      {['Tipo di richiesta', 'data', 'Periodo', 'stato', 'richiedente'].map((column) => (
-                        <th
-                          key={column}
-                          scope="col"
-                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 cursor-pointer"
-                          onClick={() => handleSort(column)}
-                        >
-                          {column.charAt(0).toUpperCase() + column.slice(1)}
-                          {sortColumn === column ? (
-                            sortDirection === 'asc' ? (
-                              <ArrowUpIcon className="h-5 w-5 inline ml-2" />
-                            ) : (
-                              <ArrowDownIcon className="h-5 w-5 inline ml-2" />
-                            )
-                          ) : null}
-                          <br />
-                          <input
-                            value={searchQueries[column]}
-                            onClick={(e) => e.stopPropagation()}
-                            onChange={handleSearchInputChange(column)}
-                            className="mt-1 px-2 py-1 w-20 border border-gray-300 rounded-md shadow-sm focus:ring-[#7fb7d4] focus:border-[#7fb7d4] sm:text-xs"
-                            placeholder=""
-                          />
-                        </th>
-                      ))}
-                      <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
-                        <span className="sr-only">Actions</span>
+                <thead>
+                  <tr>
+                    {/* Mappatura dei nomi delle colonne */}
+                    {[
+                      { key: 'location', label: 'Tipo di Richiesta' },
+                      { key: 'date', label: 'Data' },
+                      { key: 'period', label: 'Periodo' },
+                      { key: 'status', label: 'Stato' },
+                      { key: 'owner', label: 'Richiedente' }
+                    ].map(({ key, label }) => (
+                      <th
+                        key={key}
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 cursor-pointer"
+                        onClick={() => handleSort(key)}
+                      >
+                        {label}
+                        {sortColumn === key ? (
+                          sortDirection === 'asc' ? (
+                            <ArrowUpIcon className="h-5 w-5 inline ml-2" />
+                          ) : (
+                            <ArrowDownIcon className="h-5 w-5 inline ml-2" />
+                          )
+                        ) : null}
+                        <br />
+                        <input
+                          value={searchQueries[key]} // Usare le chiavi corrette per le query di ricerca
+                          onClick={(e) => e.stopPropagation()} // Impedisce l'ordinamento quando si digita
+                          onChange={handleSearchInputChange(key)}
+                          className="mt-1 px-2 py-1 w-20 border border-gray-300 rounded-md shadow-sm focus:ring-[#7fb7d4] focus:border-[#7fb7d4] sm:text-xs"
+                          placeholder=""
+                        />
                       </th>
-                    </tr>
-                  </thead>
+                    ))}
+                    <th scope="col" className="relative py-3.5 pl-3 pr-3 sm:pr-3">
+                      <span className="sr-only">Azioni</span>
+                    </th>
+                  </tr>
+                </thead>
+
                   <tbody className="divide-y divide-gray-200 bg-white">
                     {sortedCalendars.map((calendar) => (
                       <tr key={calendar.id_calendar}>
@@ -197,42 +207,36 @@ export default function HolidaysLeavesManagement({ permissions }) {
                           {calendar.period === 'morning' ? 'Mattina' : calendar.period === 'afternoon' ? 'Pomeriggio' : calendar.period}
                         </td>
                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <span
-                            className={classNames(
-                              'px-2 inline-flex text-xs leading-5 font-semibold rounded-full',
-                              calendar.status === 'In Attesa di Approvazione'
-                                ? 'bg-yellow-100 text-yellow-800'
-                                : calendar.status === 'Approvata'
-                                ? 'bg-green-100 text-green-800'
-                                : calendar.status === 'Rifiutata'
-                                ? 'bg-red-100 text-red-800'
-                                : 'bg-gray-100 text-gray-800'
-                            )}
-                          >
+                          <span className={classNames(
+                            'px-2 inline-flex text-xs leading-5 font-semibold rounded-full',
+                            calendar.status === 'In Attesa di Approvazione'
+                              ? 'bg-yellow-100 text-yellow-800'
+                              : calendar.status === 'Approvata'
+                              ? 'bg-green-100 text-green-800'
+                              : calendar.status === 'Rifiutata'
+                              ? 'bg-red-100 text-red-800'
+                              : 'bg-gray-100 text-gray-800'
+                          )}>
                             {calendar.status}
                           </span>
                         </td>
                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                           {calendar.ownerUser?.name.slice(0, 2).toUpperCase() + calendar.ownerUser?.surname.slice(0, 2).toUpperCase()}
                         </td>
-                        <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                        <td className="whitespace-nowrap py-4 px-3 text-right text-sm font-medium sm:pr-3">
                           <div className="flex items-center space-x-2">
                             <button
                               type="button"
-                              className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
-                              onClick={() => {
-                                approveCalendar(calendar.id_calendar);
-                              }}
+                              className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30"
+                              onClick={() => approveCalendar(calendar.id_calendar)}
                               disabled={calendar.status !== 'In Attesa di Approvazione'}
                             >
                               <CheckIcon className="h-5 w-4 text-gray-500" />
                             </button>
                             <button
                               type="button"
-                              className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
-                              onClick={() => {
-                                rejectCalendar(calendar.id_calendar);
-                              }}
+                              className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30"
+                              onClick={() => rejectCalendar(calendar.id_calendar)}
                               disabled={calendar.status !== 'In Attesa di Approvazione'}
                             >
                               <XMarkIcon className="h-5 w-4 text-gray-500" />
@@ -250,4 +254,4 @@ export default function HolidaysLeavesManagement({ permissions }) {
       </div>
     </>
   );
-}  
+}

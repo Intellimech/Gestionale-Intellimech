aeedbdfe3aff7b83142c8bfcb29f9a58a10ad6c8
fgervasoni7 <145766260+fgervasoni7@users.noreply.github.com>
2024-10-28 13:30:42 +0100

    fix: 🛠️ HolidayLeaves UI

--- frontend/src/components/calendar/holidaysleaves.jsx
+++ frontend/src/components/calendar/holidaysleaves.jsx
@@ -18,7 +18,7 @@ export default function HolidaysLeavesManagement({ permissions }) {
     status: '',
     owner: ''
   });
-  const [expandedGroups, setExpandedGroups] = useState({}); // Keep track of expanded groups
+  const [expandedGroups, setExpandedGroups] = useState({});
 
   useEffect(() => {
     fetchCalendars();
@@ -206,82 +206,104 @@ export default function HolidaysLeavesManagement({ permissions }) {
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
               <div className="relative">
                 <table className="min-w-full table-fixed divide-y divide-gray-300">
-                <thead>
-                  <tr>
-                    {/* Mappatura dei nomi delle colonne */}
-                    {[
-                      { key: 'location', label: 'Tipo di Richiesta' },
-                      { key: 'date', label: 'Data' },
-                      { key: 'period', label: 'Periodo' },
-                      { key: 'status', label: 'Stato' },
-                      { key: 'owner', label: 'Richiedente' }
-                    ].map(({ key, label }) => (
-                      <th
-                        key={key}
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 cursor-pointer"
-                        onClick={() => handleSort(key)}
-                      >
-                        {label}
-                        {sortColumn === key ? (
-                          sortDirection === 'asc' ? (
-                            <ArrowUpIcon className="h-5 w-5 inline ml-2" />
-                          ) : (
-                            <ArrowDownIcon className="h-5 w-5 inline ml-2" />
-                          )
-                        ) : null}
-                        <br />
-                        <input
-                          value={searchQueries[key]} // Usare le chiavi corrette per le query di ricerca
-                          onClick={(e) => e.stopPropagation()} // Impedisce l'ordinamento quando si digita
-                          onChange={handleSearchInputChange(key)}
-                          className="mt-1 px-2 py-1 w-20 border border-gray-300 rounded-md shadow-sm focus:ring-[#7fb7d4] focus:border-[#7fb7d4] sm:text-xs"
-                          placeholder=""
-                        />
+                  <thead>
+                    <tr>
+                      {[
+                        { key: 'location', label: 'Tipo di Richiesta' },
+                        { key: 'date', label: 'Data' },
+                        { key: 'period', label: 'Periodo' },
+                        { key: 'status', label: 'Stato' },
+                        { key: 'owner', label: 'Richiedente' }
+                      ].map(({ key, label }) => (
+                        <th
+                          key={key}
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 cursor-pointer"
+                          onClick={() => handleSort(key)}
+                        >
+                          {label}
+                          {sortColumn === key ? (
+                            sortDirection === 'asc' ? (
+                              <ArrowUpIcon className="h-5 w-5 inline ml-2" />
+                            ) : (
+                              <ArrowDownIcon className="h-5 w-5 inline ml-2" />
+                            )
+                          ) : null}
+                          <br />
+                          <input
+                            value={searchQueries[key]}
+                            onClick={(e) => e.stopPropagation()}
+                            onChange={handleSearchInputChange(key)}
+                            className="mt-1 px-2 py-1 w-20 border border-gray-300 rounded-md shadow-sm focus:ring-[#7fb7d4] focus:border-[#7fb7d4] sm:text-xs"
+                            placeholder=""
+                          />
                         </th>
                       ))}
+                      <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                        <span className="sr-only">Actions</span>
+                      </th>
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
                     {Object.keys(groupedCalendars).map((groupKey) => {
                       const calendarsGroup = groupedCalendars[groupKey];
                       const isExpanded = expandedGroups[groupKey];
+                      
+                      // Get details from the first request in the group
+                      const firstRequest = calendarsGroup[0];
+                      const ownerName = firstRequest.ownerUser?.name || '';
+                      const ownerSurname = firstRequest.ownerUser?.surname || '';
+                      
+                      // Get unique request types
+                      const uniqueRequestTypes = [...new Set(calendarsGroup.map(cal => cal.Location.name))];
+                      const requestTypesString = uniqueRequestTypes.join(', ');
 
                       return (
                         <Fragment key={groupKey}>
                           <tr className="bg-gray-100">
-                            <td colSpan={5} className="px-3 py-2 text-left text-sm font-semibold text-gray-900">
-                              <button
-                                className="mr-2 text-[#7fb7d4] hover:text-[#7fb7d4]"
-                                onClick={() => toggleGroup(groupKey)}
-                              >
-                                {isExpanded ? (
-                                  <ArrowUpIcon className="h-5 w-5 inline" aria-hidden="true" />
-                                ) : (
-                                  <ArrowDownIcon className="h-5 w-5 inline" aria-hidden="true" />
-                                )}
-                                {new Date(groupKey).toLocaleString()} {/* Formatted date */}
-                              </button>
-                              <span className="ml-4">
-                                {calendarsGroup.length} richieste
-                              </span>
-                            </td>
-                            <td className="text-center">
-                              <button
-                                onClick={() => approveGroup(calendarsGroup)}
-                                className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30">
-                                <CheckIcon className="h-5 w-4 text-gray-500" />
-                              
-                              </button>
-                              <button
-                                onClick={() => rejectGroup(calendarsGroup)}
-                                className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30"
-                                >
-                                  <XMarkIcon className="h-5 w-4 text-gray-500" />
-                              </button>
+                            <td colSpan="6" className="px-3 py-2 text-left text-sm font-semibold text-gray-900">
+                              <div className="flex items-center justify-between">
+                                <div className="flex items-center space-x-4">
+                                  <button
+                                    className="text-[#7fb7d4] hover:text-[#7fb7d4]"
+                                    onClick={() => toggleGroup(groupKey)}
+                                  >
+                                    {isExpanded ? (
+                                      <ArrowUpIcon className="h-5 w-5 inline" aria-hidden="true" />
+                                    ) : (
+                                      <ArrowDownIcon className="h-5 w-5 inline" aria-hidden="true" />
+                                    )}
+                                  </button>
+                                  <span>{new Date(groupKey).toLocaleString()}</span>
+                                  <span className="font-medium text-gray-700">
+                                    {ownerName} {ownerSurname}
+                                  </span>
+                                  <span className="text-gray-500">
+                                     {requestTypesString}
+                                  </span>
+                                  <span className="text-gray-500">
+                                    {calendarsGroup.length} richieste
+                                  </span>
+                                </div>
+                                <div>
+                                  <button
+                                    onClick={() => approveGroup(calendarsGroup)}
+                                    className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 mr-2"
+                                  >
+                                    <CheckIcon className="h-5 w-4 text-gray-500" />
+                                  </button>
+                                  <button
+                                    onClick={() => rejectGroup(calendarsGroup)}
+                                    className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30"
+                                  >
+                                    <XMarkIcon className="h-5 w-4 text-gray-500" />
+                                  </button>
+                                </div>
+                              </div>
                             </td>
                           </tr>
                           {isExpanded &&
+
                             calendarsGroup.map((calendar) => (
                               <tr key={calendar.id_calendar}>
                                 <td className="px-3 py-2 text-sm text-gray-500">{calendar.Location.name}</td>
